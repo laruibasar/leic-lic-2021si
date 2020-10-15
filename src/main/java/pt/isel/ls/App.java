0017121f0b766336e544769b2c2edebfe3186420
Luis Bandarra <A27969@alunos.isel.pt>
2020-10-15 22:57:28 +0100

    Start phase0-part3 sql delete

--- src/main/java/pt/isel/ls/App.java
+++ src/main/java/pt/isel/ls/App.java
@@ -1,8 +1,13 @@
 package pt.isel.ls;
 
+import org.postgresql.ds.PGSimpleDataSource;
+import java.sql.*;
+
 public class App {
     public static void main(String[] args) {
 
         System.out.println("Hello LS");
+
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
     }
 }
