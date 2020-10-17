49e6b211392c541e750c9c374792817ae024bc8b
Luis Bandarra <luis.bandarra@homestudio.pt>
2020-10-17 13:30:19 +0100

    Fix style on code

--- src/main/java/pt/isel/ls/App.java
+++ src/main/java/pt/isel/ls/App.java
@@ -1,7 +1,10 @@
 package pt.isel.ls;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import org.postgresql.ds.PGSimpleDataSource;
-import java.sql.*;
 
 public class App {
     public static void main(String[] args) {
@@ -15,34 +18,31 @@ public class App {
         ds.setPassword("test");
 
         Connection conn = null;
-        try
-        {
+        try {
             conn = ds.getConnection();
             // use connection
             // a simple select
-            PreparedStatement query = conn.prepareStatement("SELECT * FROM " +
-                            "students WHERE course = ?");
+            PreparedStatement query = conn.prepareStatement("SELECT * FROM "
+                    + "students WHERE course = ?");
             query.setInt(1, 1);
             ResultSet rs = query.executeQuery();
 
-            while (rs.next())
-            {
+            while (rs.next()) {
                 System.out.print("1: (SELECT) Result: " + rs.getInt(1) + " " + rs.getString(2));
             }
             rs.close();
             query.close();
 
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             // log error
             System.out.println(e.getMessage());
-        }
-        finally
-        {
-            if (conn != null)
-            {
-                try { conn.close(); } catch (SQLException e) {}
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    System.out.println(e.getMessage());
+                }
             }
         }
     }
