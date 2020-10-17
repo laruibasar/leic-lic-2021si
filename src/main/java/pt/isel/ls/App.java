7a47e346c641e454b711bfb561707ea336bdda76
Luis Bandarra <luis.bandarra@homestudio.pt>
2020-10-17 11:30:27 +0100

    Added simple SELECT to APP

--- src/main/java/pt/isel/ls/App.java
+++ src/main/java/pt/isel/ls/App.java
@@ -8,6 +8,42 @@ public class App {
 
         System.out.println("Hello LS");
 
-        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        /* Create a DB connection to use through our simple app */
+        PGSimpleDataSource ds = new PGSimpleDataSource();
+        ds.setDatabaseName("testdb");
+        ds.setUser("test");
+        ds.setPassword("test");
+
+        Connection conn = null;
+        try
+        {
+            conn = ds.getConnection();
+            // use connection
+            // a simple select
+            PreparedStatement query = conn.prepareStatement("SELECT * FROM " +
+                            "students WHERE course = ?");
+            query.setInt(1, 1);
+            ResultSet rs = query.executeQuery();
+
+            while (rs.next())
+            {
+                System.out.print("1: (SELECT) Result: " + rs.getInt(1) + " " + rs.getString(2));
+            }
+            rs.close();
+            query.close();
+
+        }
+        catch (SQLException e)
+        {
+            // log error
+            System.out.println(e.getMessage());
+        }
+        finally
+        {
+            if (conn != null)
+            {
+                try { conn.close(); } catch (SQLException e) {}
+            }
+        }
     }
 }
