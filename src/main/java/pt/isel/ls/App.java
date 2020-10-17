bd5bb87c39c89337138393e5694ee1da106c837b
Luis Bandarra <luis.bandarra@homestudio.pt>
2020-10-17 13:57:18 +0100

    Add DELETE option

--- src/main/java/pt/isel/ls/App.java
+++ src/main/java/pt/isel/ls/App.java
@@ -12,6 +12,7 @@ public class App {
         System.out.println("Hello LS");
 
         /* Create a DB connection to use through our simple app */
+        // TO DO: remove hardcoded definition
         PGSimpleDataSource ds = new PGSimpleDataSource();
         ds.setDatabaseName("testdb");
         ds.setUser("test");
@@ -22,26 +23,63 @@ public class App {
             conn = ds.getConnection();
             // use connection
             // a simple select
-            PreparedStatement query = conn.prepareStatement("SELECT * FROM "
-                    + "students WHERE course = ?");
-            query.setInt(1, 1);
-            ResultSet rs = query.executeQuery();
+            String query = "SELECT * FROM students";
+            PreparedStatement stmt = conn.prepareStatement(query);
+            ResultSet rs = stmt.executeQuery();
 
+            System.out.println("Before DELETE");
             while (rs.next()) {
-                System.out.print("1: (SELECT) Result: " + rs.getInt(1) + " " + rs.getString(2));
+                System.out.println("Result: "
+                        + rs.getInt(1)
+                        + " " + rs.getString(2));
             }
+
+            // a simple delete
+            conn.setAutoCommit(false);
+
+            String queryDelete = "DELETE FROM students WHERE course = ?";
+            stmt = conn.prepareStatement(queryDelete);
+            stmt.setInt(1, 2); // LEETC
+
+            int result = stmt.executeUpdate();
+
+            if (result > 0) {
+                System.out.println("Deleted from course LEETC");
+            } else {
+                System.out.println("Nothing to delete");
+            }
+
+            // Check
+            stmt = conn.prepareStatement(query);
+            rs = stmt.executeQuery();
+            System.out.println("After DELETE");
+            while (rs.next()) {
+                System.out.println("Result: "
+                        + rs.getInt(1)
+                        + " " + rs.getString(2));
+            }
+
             rs.close();
-            query.close();
+            stmt.close();
 
         } catch (SQLException e) {
+            try {
+                conn.rollback();
+            } catch (SQLException e1) {
+                e1.getStackTrace();
+            }
+
             // log error
             System.out.println(e.getMessage());
         } finally {
             if (conn != null) {
                 try {
+                    conn.rollback(); // Change to conn.commit() to save
                     conn.close();
+                    System.out.println("Sucess on try delete but rollbacked");
                 } catch (SQLException e) {
                     System.out.println(e.getMessage());
+                    e.getStackTrace();
                 }
             }
         }
