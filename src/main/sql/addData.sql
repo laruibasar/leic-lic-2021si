205c68e7ff57c2b48ce0087346823e3c9e985258
Luis Bandarra <A27969@alunos.isel.pt>
2020-10-17 16:43:03 +0100

    Add test data

---
+++ src/main/sql/addData.sql
@@ -0,0 +1,2 @@
+insert into courses(name) values ('LEIC'), ('LEETC'), ('LEEE');
+insert into students(course, number, name) values (1, 12345, 'Ana'), (2, 12344, 'Bruno'), (3, 12334, 'Clara'), (1, 12234, 'David');
