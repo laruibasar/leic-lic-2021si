30844f6937b0969890c7d4e3ba6e7a50a4354278
Daniel Azevedo <azevedo_daniel1994@hotmail.com>
2020-10-23 19:47:50 +0100

    Dependencies update

--- src/main/java/pt/isel/ls/utils/Command.java
+++ src/main/java/pt/isel/ls/utils/Command.java
@@ -5,6 +5,7 @@ public class Command {
     private String method;
     private String path;
     private String [] params;
+    private Parser p;
 
     /***
      * A Parser object is responsible for handling the information contained
@@ -13,10 +14,10 @@ public class Command {
      * the information extracted by the command's Parser.
      */
     public Command(String cmd) {
-        Parser p = new Parser(cmd);
-        this.method = p.getMethod();
-        this.path = p.getPath();
-        this.params = p.getParams();
+        p = new Parser();
+        this.path = getPath(cmd);
+        this.method = getMethod(cmd);
+        this.params = getParams(cmd);
 
         //for test
         System.out.print("Method = "+method +" ");
@@ -24,5 +25,31 @@ public class Command {
             System.out.print("Parameter: " + par +" ");
         }
         System.out.print(" Path = "+ path +"\n\n");
+    }
+
+    public String getPath(String cmd) {
+        String[] pathExtractor = cmd.split(" ");
+        return path = pathExtractor[1];
+    }
+
+    public String getMethod(String cmd) {
+        String[] methodExtractor = cmd.split(" ");
+        return method = methodExtractor[0];
+    }
+
+    public String[] getParams(String cmd){
+        String[] input = cmd.split(" ");
+        if(method.equals("GET")) {
+            params = p.paramsFromGET(input);
+        }else if (method.equals("POST")){
+            params = p.paramsFromPOST(input);
+        }else {
+            Exit();
+        }
+        return params;
+    }
+
+    public void Exit(){
+        System.exit(0);
     }
 }
--- src/test/java/pt/isel/ls/utils/CommandTest.java
+++ src/test/java/pt/isel/ls/utils/CommandTest.java
@@ -10,6 +10,7 @@ public class CommandTest {
         Command cmd2 = new Command("POST /movies/240/ratings rating=8");
         Command cmd3 = new Command("POST /movies/{mid}/reviews uid=546&reviewSummary=bad&review=horriblemoviemustnotbeseen&rating=0");
         Command cmd4 = new Command("GET /movies/155/reviews");
+        Command cmd5 = new Command("GET /movies/156/reviews/513");
     }
 
 }
