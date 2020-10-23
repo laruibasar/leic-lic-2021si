20fe4bf5919be4ed4f091d116749ee1bbe41ab19
Luis Bandarra <luis.bandarra@homestudio.pt>
2020-10-23 21:29:16 +0100

    Add router for routing commands

--- src/main/java/pt/isel/ls/config/Router.java
+++ src/main/java/pt/isel/ls/config/Router.java
@@ -1,30 +1,57 @@
 package pt.isel.ls.config;
 
 import pt.isel.ls.services.Handler;
+import pt.isel.ls.utils.Command;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
 import java.util.List;
 
 public class Router {
 
     /* store the application available handlers */
+    //private static Hashtable<String, Handler> getHandlers;
+    //private static Hashtable<String, Handler> postHandlers;
+
     private static List<Handler> handlers;
 
     public Router() throws RouterException {
+        //getHandlers = new Hashtable<>();
+        //postHandlers = new Hashtable<>();
         handlers = new ArrayList<>();
     }
 
-    /* TODO: this method is to add a handler to our list of routing allowed.
-     *       We care more with store the class of Handler
-     *       than the setup of handler
-     */
-    public static void addHandler(String method, String template,
-                                  String parameters) {
-        // TODO: code to create a handler and store in list
+    /*
+    public static void addHandler(String method, String path,
+                                  Handler handler) throws RouterException {
+        switch (method) {
+            case "post":
+                postHandlers.put(path, handler);
+                break;
+            case "get":
+                getHandlers.put(path, handler);
+                break;
+            default:
+                throw new RouterException("Router: invalid method " + method);
+        }
+    }
+    */
+
+    public static void addHandler(String method, String path, Handler handler) {
+        Command template = new Command(method, path);
+        handler.setTemplate(template);
+        handlers.add(handler);
     }
 
-    public static Handler getHandler(String request) {
-        // TODO: code to search and return new instance of and Handler
-        return null;
+    public static Handler getHandler(Command command) throws RouterException {
+        Iterator<Handler> it = handlers.iterator();
+        while (it.hasNext()) {
+            Handler handler = it.next();
+            if (handler.getTemplate().equals(command)) {
+                return handler;
+            }
+        }
+        throw new RouterException("Router: invalid command");
     }
 }
--- src/main/java/pt/isel/ls/services/Handler.java
+++ src/main/java/pt/isel/ls/services/Handler.java
@@ -1,7 +1,15 @@
 package pt.isel.ls.services;
 
+import pt.isel.ls.utils.Command;
+
 public abstract class Handler {
-    public int execute() {
-        return 0;
+    private Command template;
+
+    public void setTemplate(Command template) {
+        this.template = template;
+    }
+
+    public Command getTemplate() {
+        return template;
     }
 }
