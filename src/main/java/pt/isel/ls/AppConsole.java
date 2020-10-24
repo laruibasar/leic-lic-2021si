712eae059b90062eda1e8dae8f852766c2d57122
Luis Bandarra <luis.bandarra@homestudio.pt>
2020-10-24 19:20:55 +0100

    Start run command logic

--- src/main/java/pt/isel/ls/AppConsole.java
+++ src/main/java/pt/isel/ls/AppConsole.java
@@ -3,9 +3,25 @@ package pt.isel.ls;
 public class AppConsole {
     public static void run() {
         System.out.println("Run interactive");
+        /* TODO: insert logic to interpret args and run the command:
+         *  1. Ask input from user
+         *  2. Interpret input
+         *  2. If EXIT / terminate
+         *  3. Create Command and get Handler that run the command
+         *  4. Show results
+         *  5. goto 1.
+         */
     }
 
     public static void runOnce(String[] args) {
         System.out.println("Run once");
+        /*
+         * TODO: insert logic to interpret args and run the command:
+         *  1. Interpret args
+         *  2. If EXIT / terminate
+         *  3. Create Command and get Handler that run the command
+         *  4. Show results
+         *  5. Exit
+         */
     }
 }
