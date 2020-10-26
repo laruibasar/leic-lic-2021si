b213242585246be88c4309106eca0fbec102c37e
Luis Bandarra <luis.bandarra@homestudio.pt>
2020-10-26 19:59:10 +0000

    Add first routing handler

--- src/main/java/pt/isel/ls/services/GetUsersHandler.java
+++ src/main/java/pt/isel/ls/services/GetUsersHandler.java
@@ -6,6 +6,7 @@ public class GetUsersHandler extends Handler implements IHandler {
 
     @Override
     public CommandResult execute() {
+        System.out.println("Handler speaking: Getting all users");
         return null;
     }
 }
