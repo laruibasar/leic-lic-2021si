a25fc16411d76de269049717324b2b694cd8123a
Luis Bandarra <A27969@alunos.isel.pt>
2020-11-03 19:35:43 +0000

    Fix styles in Movie

--- src/main/java/pt/isel/ls/model/Movie.java
+++ src/main/java/pt/isel/ls/model/Movie.java
@@ -9,27 +9,35 @@ public class Movie {
     private String castAndDirectors;
 
     public Movie(int mid, String title, int year) {
-
         this.mid = mid;
         this.title = title;
         this.year = year;
     }
 
-    public int getMid() { return mid; }
+    public int getMid() {
+        return mid;
+    }
+
     public String getTitle() {
         return title;
     }
-    public int getYear() { return year; }
+
+    public int getYear() {
+        return year;
+    }
 
     public void setGenre(String genre) {
         this.genre = genre;
     }
+
     public String getGenre() {
         return genre;
     }
+
     public void setCastAndDirectors(String castAndDirectors) {
         this.castAndDirectors = castAndDirectors;
     }
+
     public String getCastAndDirectors() {
         return castAndDirectors;
     }
