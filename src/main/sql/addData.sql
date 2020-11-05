f09fb8b6c41b8af0d6198c8c55bd4c652efa312c
Tiago Conceição <A47611@alunos.isel.pt>
2020-11-05 16:19:23 +0000

    Reajusting addData script to new database model

--- src/main/sql/addData.sql
+++ src/main/sql/addData.sql
@@ -1,34 +1,29 @@
-insert into users(uid, fname, lname, email ) values
-(1, 'Mike', 'Albuquerque', 'Mike-Alb@gmail.com'),
-(2, 'Alice', 'Nahuw', 'Ali.chi@hotmail.com'),
-(3, 'Alvaro', 'Abajo', 'A.Acima@gmail.com'),
-(4, 'Maria', 'Antonieta', 'MariaAnt-1755@gmail.com'),
-(5, 'Vasco', 'Couves', 'ze-da-horta@hotmail.com');
+insert into users(uid, name, email ) values
+(1, 'Mike Albuquerque', 'Mike-Alb@gmail.com'),
+(2, 'Alice Nahuw', 'Ali.chi@hotmail.com'),
+(3, 'Alvaro Abajo', 'A.Acima@gmail.com'),
+(4, 'Maria Antonieta', 'MariaAnt-1755@gmail.com'),
+(5, 'Vasco Couves', 'ze-da-horta@hotmail.com');
 
-insert into movies(name, age, genre, castAndDirectors) values
-('Gladiator', 2000, 'Action, Adventure, Drama', 'Ridley Scott, Russell Crowe, Joaquin Phoenix, Connie Nielsen'),
-('The Fast and the Furious', 2001, 'Action, Adventure, Drama', 'Rob Cohen,  Vin Diesel, Paul Walker, Michelle Rodriguez' ),
-('Finding Nemo', 2003, 'Animation, Adventure, Comedy', 'Andrew Stanton, Lee Unkrich, Albert Brooks');
+insert into movies(name, age) values
+('Gladiator', 2000),
+('The Fast and the Furious', 2001),
+('Finding Nemo', 2003);
 
 insert into reviews(rid, summary, completeReview, rating, movie, movieCritic) values
 (1234, 'Edge of Your Seat Fun!','Great Story! Great Writing! Great Acting! Great Directing! This movie has it all.',
- 111, 159, 1),
- (1125, 'Great','Gladiator is a historical epic from director Ridley Scott.',110, 159,4)
+ 5, 1, 1),
+ (1125, 'Great','Gladiator is a historical epic from director Ridley Scott.',4, 1,4)
 (1236, 'Swimming with Sharks is a Whale of a Time',
-'The voice cast are capable and cannily chosen, from young Alexander Gould as Nemo.', 112, 160, 2),
+'The voice cast are capable and cannily chosen, from young Alexander Gould as Nemo.', 3, 2, 2),
  (1237, 'Yes, its THAT good !!', 'I will be totally honest and confirm to you that everything what they say about this movie is true.',
- 114, 160, 3),
+ 5, 2, 3),
  (1238, 'A solid entertainment weel done action heist movie',
- 'The Fast and the Furious is a gritty and gratifying cheap thrill, Rob Cohens high-octane hot-car.',113, 161, 5);
+ 'The Fast and the Furious is a gritty and gratifying cheap thrill, Rob Cohens high-octane hot-car.',3, 3, 5);
 
- insert into movies_reviews(mid, name, age) values
- (159, 'Gladiator', 2000),
- (160, 'Finding Nemo', 2003),
- (161, 'The Fast and the Furious', 2001);
-
- insert into ratings(rid, rating) values
- (110, 4),
- (111, 5),
- (112, 3),
- (113, 3),
- (114, 5);
+ insert into ratings(rid, rating, movie) values
+ (1, 4),
+ (2, 5),
+ (3, 3),
+ (4, 3),
+ (5, 5);
