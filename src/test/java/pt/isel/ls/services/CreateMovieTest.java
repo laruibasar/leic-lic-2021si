8d7d7776850d11a693f283805226844f82fbf442
Luis Bandarra <luis.bandarra@homestudio.pt>
2020-11-07 14:34:37 +0000

    Movie handler test complete

--- src/test/java/pt/isel/ls/services/CreateMovieTest.java
+++ src/test/java/pt/isel/ls/services/CreateMovieTest.java
@@ -11,6 +11,7 @@ import pt.isel.ls.utils.Command;
 import pt.isel.ls.utils.CommandResult;
 import pt.isel.ls.utils.Method;
 import pt.isel.ls.utils.Parameters;
+import pt.isel.ls.utils.ParametersExceptions;
 import pt.isel.ls.utils.Path;
 
 import static org.junit.Assert.assertEquals;
@@ -20,7 +21,8 @@ import java.sql.SQLException;
 public class CreateMovieTest {
 
     @Test
-    public void insert_correct_test() throws DataConnectionException, SQLException {
+    public void insert_correct_test() throws DataConnectionException,
+            SQLException, ParametersExceptions {
         AppConfig.setup();
 
         Parameters params = new Parameters();
@@ -37,8 +39,41 @@ public class CreateMovieTest {
         }
     }
 
-    @Test
-    public void insert_incorrect_params_test() {
+    @Test (expected = DataConnectionException.class)
+    public void insert_duplicate_year_title_fail() throws DataConnectionException,
+            SQLException, ParametersExceptions {
+        AppConfig.setup();
 
+        Parameters params = new Parameters();
+        params.setValues("title=The+Godfather&releaseYear=1972");
+        Command cmd = new Command(Method.POST, new Path("/movies"), params);
+        CreateMovieHandler handler = new CreateMovieHandler();
+        CommandResult rs = handler.execute(cmd);
+
+        assertEquals(1, rs.getStatus());
+        for (Model test : rs) {
+            Movie testMovie = (Movie) test;
+            assertEquals("The Godfather", testMovie.getTitle());
+            assertEquals(1972, testMovie.getYear());
+        }
+    }
+
+    @Test (expected = ParametersExceptions.class)
+    public void missing_parameters() throws DataConnectionException,
+            SQLException, ParametersExceptions {
+        AppConfig.setup();
+
+        Parameters params = new Parameters();
+        params.setValues("title=The+Godfather:+part+II");
+        Command cmd = new Command(Method.POST, new Path("/movies"), params);
+        CreateMovieHandler handler = new CreateMovieHandler();
+        CommandResult rs = handler.execute(cmd);
+
+        assertEquals(1, rs.getStatus());
+        for (Model test : rs) {
+            Movie testMovie = (Movie) test;
+            assertEquals("The Godfather", testMovie.getTitle());
+            assertEquals(1972, testMovie.getYear());
+        }
     }
 }
