e960872060cf32eb456d16af61e2b9e0807bf01d
Daniel Azevedo <azevedo_daniel1994@hotmail.com>
2020-11-27 23:38:53 +0000

    Class print CommandResult

---
+++ src/main/java/pt/isel/ls/FileWriter.java
@@ -0,0 +1,9 @@
+package pt.isel.ls;
+
+
+public class FileWriter extends Exception {
+
+    public FileWriter(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/pt/isel/ls/PrintResults.java
@@ -0,0 +1,56 @@
+package pt.isel.ls;
+
+import pt.isel.ls.utils.CommandResult;
+import pt.isel.ls.utils.Header;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class PrintResults {
+
+    private final CommandResult commandResult;
+    private final Header header;
+
+    public PrintResults(CommandResult commandResult, Header header) {
+        this.commandResult = commandResult;
+        this.header = header;
+    }
+
+    @Override
+    public String toString() {
+        String printType, textType;
+        printType = header.headerValues.get("file-name");
+        textType = header.headerValues.get("accept");
+        if (printType.equals("standard output")) {
+            return consolePrint(textType);
+        }
+        return filePrint(printType, textType);
+    }
+
+    public String consolePrint(String textType) {
+        if (textType.equals("text/html")) {
+            return commandResult.printHTML();
+        }
+        return commandResult.printPlainText();
+    }
+
+    //TODO: create correct exception
+    public String filePrint(String fileName, String textType) {
+        File file = new File(fileName);
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (textType.equals("HTML")) {
+            fw.write(commandResult.printHTML());
+        } else {
+            fw.write(commandResult.printPlainText());
+        }
+        return "File "
+                + fileName
+                + "created";
+    }
+}
