e97cd1396aff6853566e119f9ce5e7461c4378c6
Tiago Conceição <A47611@alunos.isel.pt>
2020-11-28 19:31:43 +0000

    Implementation of CommandResult for Option

---
+++ src/main/java/pt/isel/ls/view/results/OptionResult.java
@@ -0,0 +1,56 @@
+package pt.isel.ls.view.results;
+
+import pt.isel.ls.config.Router;
+import pt.isel.ls.utils.Command;
+import pt.isel.ls.view.html.Html;
+import pt.isel.ls.view.html.body.Body;
+import pt.isel.ls.view.html.body.Bullets;
+import pt.isel.ls.view.html.body.Table;
+import pt.isel.ls.view.html.head.Head;
+import pt.isel.ls.view.html.head.Title;
+
+import java.util.ArrayList;
+
+public class OptionResult extends CommandResult {
+
+    private Router router;
+
+    public OptionResult(Router router) {
+        this.router = router;
+    }
+
+    @Override
+    public String printHtml() {
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        for (Command c : router) {
+            arrayList.add(router.getHandler(c).getDescription() + "\n"
+                    + "Command: " + c.toString() + "\n");
+        }
+
+        String[] bullets = new String[]{};
+        arrayList.toArray(bullets);
+
+        Html h = new Html(
+                new Head(
+                        new Title("User All Reviews")
+                ),
+                new Body(
+                        new Bullets(
+                                bullets
+                        )
+                )
+        );
+        return h.toString();
+    }
+
+    @Override
+    public String printPlainText() {
+        StringBuilder sb = new StringBuilder();
+        for (Command c : router) {
+            sb.append(router.getHandler(c).getDescription() + "\n"
+                    + "Command: " + c.toString() + "\n");
+        }
+        return sb.toString();
+    }
+}
