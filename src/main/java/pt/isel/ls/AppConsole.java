5f339d61faa0c51240f4aee6fee471027ad4602d
Luis Bandarra <luis.bandarra@homestudio.pt>
2021-01-03 17:02:00 +0000

    Change: add single class that deals with commands

--- src/main/java/pt/isel/ls/AppConsole.java
+++ src/main/java/pt/isel/ls/AppConsole.java
@@ -1,16 +1,11 @@
 package pt.isel.ls;
 
-import pt.isel.ls.handlers.common.Handler;
 import pt.isel.ls.handlers.common.HandlerException;
 import pt.isel.ls.results.ExitResult;
 import pt.isel.ls.utils.Command;
-import pt.isel.ls.config.AppConfig;
 import pt.isel.ls.config.RouterException;
 import pt.isel.ls.results.CommandResult;
 import pt.isel.ls.utils.Header;
-import pt.isel.ls.utils.Method;
-import pt.isel.ls.utils.Parameters;
-import pt.isel.ls.utils.Path;
 import pt.isel.ls.view.PrintResults;
 
 import java.util.Scanner;
@@ -48,14 +43,14 @@ public class AppConsole {
         }
 
         try {
-            Command cmd = setCommand(args);
+            Command cmd = AppCommand.setCommand(args);
 
-            CommandResult result = runCommand(cmd);
+            CommandResult result = AppCommand.runCommand(cmd);
             if (result.getClass() == ExitResult.class) {
                 return false;
             }
 
-            showResults(result, cmd.getHeader());
+            showResults(cmd, result);
         } catch (RouterException | HandlerException e) {
             System.out.println("ERROR " + e.getMessage() + "\n");
         }
@@ -63,44 +58,8 @@ public class AppConsole {
         return true;
     }
 
-    private static Command setCommand(String[] args) throws RouterException {
-        Method method = Method.getMethod(args[0].toUpperCase());
-        if (method == null) {
-            throw new RouterException("Command method invalid " + args[0]);
-        }
-
-        Path path = new Path(args[1]);
-        Header header = new Header();
-        Parameters params = new Parameters();
-
-        if (args.length == 3) {
-            if (args[2].contains("accept:") || args[2].contains("file-name:")) {
-                header.setValues(args[2]);
-            } else {
-                params.setValues(args[2]);
-            }
-        } else if (args.length == 4) {
-            header.setValues(args[2]);
-            params.setValues(args[3]);
-        }
-        Command cmd = new Command(method, path, header, params);
-
-        try {
-            cmd.setTemplate(AppConfig.getRouter().findTemplate(cmd));
-        } catch (RouterException e) {
-            throw new RouterException(e.getMessage());
-        }
-
-        return cmd;
-    }
-
-    private static CommandResult runCommand(Command cmd) throws RouterException,
-            HandlerException {
-        Handler handler = AppConfig.getRouter().findHandler(cmd);
-        return handler.execute(cmd);
-    }
-
-    private static void showResults(CommandResult cr, Header hd) {
+    private static void showResults(Command cmd, CommandResult cr) {
+        Header hd = cmd.getHeader();
         System.out.println(new PrintResults(cr, hd).toString());
     }
 }
